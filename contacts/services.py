4a7fde3146b11555748ad65df96ee8b51ee47ae7
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-03-10 21:06:49 +0100

    Update services.py

--- contacts/services.py
+++ contacts/services.py
@@ -2,6 +2,6 @@ from contacts.models import Contact
 
 
 def save_contact(contact: Contact):
-    # TODO Snimiti kontakt u datoteku contacts.json
+    # TODO Snimiti kontakt u datoteku contacts.json koji se nalazi u folderu files
     # !!! Obratiti paznju da uvijek radite snimanje liste kontakata
     pass
